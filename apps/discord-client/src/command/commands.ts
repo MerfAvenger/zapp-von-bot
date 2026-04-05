2aa7ac984a0a3683526851ad590ae6c46919bdd9
Connor Barthelmie <connor.barthelmie@gmail.com>
2026-04-05 04:26:33 +0100

    Remove sneaky users commands

--- apps/discord-client/src/command/commands.ts
+++ apps/discord-client/src/command/commands.ts
@@ -1,6 +1,5 @@
 import settings from "./commands/settings/settings";
 import messageTheAdmiralsCommand from "./commands/message-the-admirals/messageTheAdmirals";
-import userCommands from "./commands/users/users";
 import admin from "./commands/admin/admin";
 
-export default [admin, settings, messageTheAdmiralsCommand, userCommands];
+export default [admin, settings, messageTheAdmiralsCommand];
